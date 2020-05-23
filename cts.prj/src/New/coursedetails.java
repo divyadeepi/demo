85ae3d4f9a65b9987d5b074cfcef56ba6d38479f
divyadeepi <Madesh@DESKTOP-62M2QH8>
2020-05-23 17:16:37 +0530

    First commit uploading all files

---
+++ cts.prj/src/New/coursedetails.java
@@ -0,0 +1,31 @@
+package New;
+
+public class coursedetails {
+	
+	public void SeleniumCourse() {
+		
+		System.out.println("selenium");
+	}
+	
+	public void javacourse() {
+		
+		System.out.println("java");
+		
+	}
+	
+	public void pythoncourse()
+	{
+		System.out.println("python");
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		coursedetails obj=new coursedetails();
+		obj.SeleniumCourse();
+		obj.javacourse();
+		obj.pythoncourse();
+		}
+
+}
